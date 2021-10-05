cab46bdde7b7f6c494d51652ba910469c04306a8
Kartik Pathak <55117106+KP-MIT@users.noreply.github.com>
2021-10-06 04:00:53 +1100

    commit implemented ride section

--- App.js
+++ App.js
@@ -1,50 +1,22 @@
-import React from "react";
-import { KeyboardAvoidingView, Platform, Text, View } from "react-native";
+import React from 'react';
+import { store } from "./app/redux/store";
 import { Provider } from "react-redux";
-import HomeScreen from "./screens/HomeScreen";
-import MapScreen from "./screens/MapScreen";
-import { store } from "./store";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import "react-native-gesture-handler";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-
-// 1. set up Redux
+import AppNavigator from './app/navigation/AppNavigator';
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { KeyboardAvoidingView, Platform } from 'react-native'; 
 
 export default function App() {
-  const Stack = createStackNavigator();
-  //   HomeScreen: {HomeScreen},
-  //   MapScreen: {MapScreen}
-  // })
   return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <SafeAreaProvider>
-          <KeyboardAvoidingView
-              behavior={Platform.OS === "ios" ? "padding" : "height"}
-              style={{ flex:1 }}
-              keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
-          >
-
-            <Stack.Navigator>
-              <Stack.Screen
-                name="HomeScreen"
-                component={HomeScreen}
-                options={{
-                  headerShown: false,
-                }}
-              />
-              <Stack.Screen
-                name="MapScreen"
-                component={MapScreen}
-                options={{
-                  headerShown: false,
-                }}
-              />
-            </Stack.Navigator>
-          </KeyboardAvoidingView>
-        </SafeAreaProvider>
-      </NavigationContainer>
-    </Provider>
+    <SafeAreaProvider>
+      <Provider store={store}>
+        <KeyboardAvoidingView 
+          style={{ flex: 1 }}
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          keyboardVerticalOffset={Platform.OS === 'ios' ? -64 : 0}
+        >
+          <AppNavigator />
+        </KeyboardAvoidingView>
+      </Provider>
+    </SafeAreaProvider>
   );
 }
